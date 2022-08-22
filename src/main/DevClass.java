8ef23ad58a9dbce7ebbb88b632aa01ce2b3377dc
Swethakrishanraj <88270356+Swethakrishanraj@users.noreply.github.com>
2022-08-22 10:52:46 +0530

    Added a dash instead of plus
    Added a dash instead of plus

--- src/main/DevClass.java
+++ src/main/DevClass.java
@@ -2,7 +2,7 @@ package main;
 
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Devclass+stream2learn");
+        System.out.println("Devclass-stream2learn");
         //checkin from stream2learn
     }
 }
