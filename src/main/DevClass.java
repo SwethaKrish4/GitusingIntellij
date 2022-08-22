1cf88699d3e809125d68ec7695da5f40c89d81fd
Swethakrishanraj <88270356+Swethakrishanraj@users.noreply.github.com>
2022-08-22 10:40:16 +0530

    Update DevClass.java

--- src/main/DevClass.java
+++ src/main/DevClass.java
@@ -2,6 +2,7 @@ package main;
 
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Devclass");
+        System.out.println("Devclass+stream2learn");
+        //checkin from stream2learn
     }
 }
